82571f920937cb8b890a2aa24192b60f2f98de9b
Julia Yoo <julia.mfy@gmail.com>
2024-12-12 11:31:03 +0000

    cv first commit

---
+++ FINAL/cv.js
@@ -0,0 +1,28 @@
+
+     
+    getData();
+
+
+async function getData() {
+    try {
+        // Fetch data from Google Sheets
+        const x = "https://opensheet.elk.sh/1v-hTX1tX15527Q1AqSBpcpnqoMv7pg9mI87SCj7VYwU/1";
+        let response = await fetch(x);
+        let data = await response.json();
+        console.log(data);
+        // add each route to map from home folder 
+        const page = document.getElementById("textcontainer");
+        data.forEach((line) => {
+            
+            if(!line.text == ""){
+                page.innerHTML += line.text;
+                page.innerHTML += "<br>";
+            }
+        }
+    
+    );
+    page.innerHTML += "<br><br><br>";
+    } catch (error) {
+    console.error("Error:", error);
+    }
+   };

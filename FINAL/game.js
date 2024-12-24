d5e8a391c56e7a6e5bcba3cd4a565151bbda6cb6
Julia Yoo <julia.mfy@gmail.com>
2024-12-23 23:09:04 -0500

    ughhh

--- FINAL/game.js
+++ FINAL/game.js
@@ -4,7 +4,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getStorage, ref, uploadString, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-storage.js";
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
 
 // Your web app's Firebase configuration
 
